6801e9403960101aa55aa509c646a51bc44e47d2
Darya Trufanova <102416136+daryatruf@users.noreply.github.com>
2024-09-22 19:19:55 +0300

    Добавлен тест на проверку поиска

---
+++ test_search.py
@@ -0,0 +1,8 @@
+import pytest
+from selene import browser, be, have
+
+
+def test_search():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
